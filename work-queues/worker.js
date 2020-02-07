de8d64b98186bb7a1a0a29642a8e307a4adfc712
Tatas Fachrul <tatasfachrul@gmail.com>
2020-02-07 13:24:39 +0700

    work queue

---
+++ work-queues/worker.js
@@ -0,0 +1,32 @@
+const amqp = require('amqplib/callback_api')
+
+amqp.connect('amqp://localhost', (error0, connection) => {
+  if (error0) throw error0
+
+  connection.createChannel((error1, channel) => {
+    if (error1) throw error1
+
+    const queue = 'task_queue'
+
+    channel.assertQueue(queue, {
+      durable: true
+    })
+
+    channel.prefetch(1)
+
+    console.log(' [*] Waiting for messages in %s. To exit press CTRL+C', queue)
+
+    channel.consume(queue, (msg) => {
+      const secs = msg.content.toString().split('.').length - 1
+
+      console.log(' [x] Received %s', msg.content.toString())
+
+      setTimeout(() => {
+        console.log(' [x] Done')
+        channel.ack(msg)
+      }, secs * 1000)
+    }, {
+      noAck: false
+    })
+  })
+})
